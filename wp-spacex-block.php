1d998412d568d5fe7e330960d228754f4d113374
Fathan Fisabililah <fathanrbe@gmail.com>
2023-02-12 12:35:57 +0700

    Add package json file, update gitignore and add plugin information

---
+++ wp-spacex-block.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Spacex Grid Block
+ * Description:       Block rendering a card with spacex external API data information.
+ * Requires at least: 6.0
+ * Requires PHP:      8.0
+ * Version:           1.0.0
+ * Author:            Fathan Fisabililah
+ * Author URI:        https://github.com/fathanfi
+ * Text Domain:       wp-spacex-block
+ *
+ * @package           SpacexBlock
+ */
+
+namespace FFI\SpacexBlock;
+
+const MAIN_FILE = __FILE__;
+const MAIN_DIR  = __DIR__;
+
+require MAIN_DIR . '/vendor/autoload.php';
